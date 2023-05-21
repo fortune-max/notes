3f4b9d06560009a522ee0b25c78bb82669ddff71
fortune-max <fortmax120@gmail.com>
2023-05-21 16:16:23 +0200

    add default port if none in env

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from './my-express';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
+import { DEFAULT_PORT } from './constants';
 
 config();
+const port = process.env.PORT || DEFAULT_PORT;
 
 mongoose.connect(process.env.MONGODB_URL as string).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log('server is running on port', process.env.PORT);
+  app.listen(port, () => {
+    console.log('server is running on port', port);
   });
 }).catch((err) => {
   console.log(err);
